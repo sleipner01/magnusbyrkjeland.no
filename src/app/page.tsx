13cf33baa9276e425ac978fe2fc85363af9d5aab
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2025-01-11 20:32:13 +0100

    chore: adding text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,10 @@ import Link from "next/link";
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center p-8 sm:p-24">
-			<div className="max-w-lg animate-rollOutLeftAndDown absolute" id="typewriter">
+			<div
+				className="max-w-lg animate-rollOutLeftAndDown absolute"
+				id="typewriter"
+			>
 				<h1 className="text-4xl font-bold overflow-hidden border-r-4 border-black dark:border-white whitespace-nowrap animate-typewriter inline-block">
 					Magnus Byrkjeland<span className="animate-blink">|</span>
 				</h1>
@@ -22,10 +25,14 @@ export default function Home() {
 				className="max-w-lg animate-rollInLeftAndUp opacity-0 absolute"
 				id="main-content"
 			>
-				<h2 className="text-3xl font-bold">Information coming...</h2>
+				<h2 className="text-3xl font-bold">👋🏽 Hello there!</h2>
 				<div className="mt-4">
-					<p>Check this out in the meantime:</p>
-					<div className="flex gap-4 mt-2">
+					<p>
+						I&apos;ll be brutally honest. My Github and Linkedin will always be
+						more informative than this website.
+					</p>
+					<p>If you want to stalk me, have a look below:</p>
+					<div className="flex gap-4 mt-6">
 						<Link
 							href="https://github.magnusbyrkjeland.no"
 							target="_blank"
