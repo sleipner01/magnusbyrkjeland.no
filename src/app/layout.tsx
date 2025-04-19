acd26d71244ef9acfc5b53d4671b6b9117fb4f02
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2025-04-19 13:05:44 +1000

    feat: adding favicon
    adding coffee favicon, because why not.
    Dark and light version depending on user preference

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,29 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import iconLight from "@/../public/images/light-icon.png";
+import iconDark from "@/../public/images/dark-icon.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-	title: "Magnus Byrkjeland",
-	description: "Magnus Byrkjeland - Software Developer",
-};
+export async function generateMetadata(): Promise<Metadata> {
+	return {
+		title: "Magnus Byrkjeland",
+		description: "Magnus Byrkjeland - Software Developer",
+		icons: [
+			{
+				media: "(prefers-color-scheme: light)",
+				url: iconDark.src,
+				type: "image/png",
+			},
+			{
+				media: "(prefers-color-scheme: dark)",
+				url: iconLight.src,
+				type: "image/png",
+			},
+		],
+	};
+}
 
 export default function RootLayout({
 	children,
@@ -18,7 +34,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>{children}</body>
-      <Analytics />
+			<Analytics />
 		</html>
 	);
 }
