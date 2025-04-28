d8bde7c43dcaac405686a3374bb967b89529cef8
Magnus Byrkjeland <magnus.e.b@hotmail.com>
2025-04-28 16:36:58 +0800

    chore: favicon setup
    adding apple and google icons.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,19 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import iconLight from "@/../public/images/light-icon.png";
-import iconDark from "@/../public/images/dark-icon.png";
+import iconLight from "@/../public/icons/light-icon.png";
+import iconDark from "@/../public/icons/dark-icon.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export async function generateMetadata(): Promise<Metadata> {
 	return {
 		title: "Magnus Byrkjeland",
-		description: "Magnus Byrkjeland - Software Developer",
+		description: "Magnus Byrkjeland · Developer",
+		creator: "Magnus Byrkjeland",
+		robots: "index, follow",
+		keywords: "portfolio, website, byrkj",
+		openGraph: {},
 		icons: [
 			{
 				media: "(prefers-color-scheme: light)",
@@ -22,7 +26,22 @@ export async function generateMetadata(): Promise<Metadata> {
 				url: iconLight.src,
 				type: "image/png",
 			},
+
+			{
+				url: "/icons/android-chrome-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				url: "/icons/android-chrome-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
 		],
+		appleWebApp: {
+			capable: true,
+			title: "Byrkj",
+		},
 	};
 }
 
---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+	return {
+		name: "Magnus Byrkjeland",
+		short_name: "Byrkj",
+		description: "Magnus Byrkjeland · Developer",
+		start_url: "/",
+		display: "standalone",
+		background_color: "#fff",
+		theme_color: "#fff",
+		icons: [
+			{
+				src: "/icons/android-chrome-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "/icons/android-chrome-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
+		],
+	};
+}
